501616600430deee152bc0846c3196be4d2d074d
Frédéric MEUROU <frederic@meurou.com>
2020-09-16 16:27:51 +0200

    Defaults to False for DEBUG

--- deployment/dev/api/settings.py
+++ deployment/dev/api/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = ''
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     os.environ.get('GEOCURRENCY_SERVICE_URL', '127.0.0.1'),
