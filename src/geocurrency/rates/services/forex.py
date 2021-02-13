3fae941444007d9dabfb846e16d59031be354145
Frédéric Meurou <fm@peabytes.me>
2021-02-13 14:37:18 +0100

    Feature/rate finder (#11)
    * use shortest path algorithm
    
    * Fix tests on Pelias with an external server
    
    * fixed pelias tests
    
    * Test find_rate with basic, chained, mixed and overriden rates

--- src/geocurrency/rates/services/forex.py
+++ src/geocurrency/rates/services/forex.py
@@ -10,12 +10,25 @@ from . import RateService, RatesNotAvailableError
 
 
 class ForexService(RateService):
+    """
+    Forex Rate service
+    """
 
     def available_currencies(self) -> Iterator:
+        """
+        List available currencies for this service
+        """
         rates = self.fetch_rates(base_currency='USD')
         return [r['currency'] for r in rates]
 
     def _fetch_all_rates(self, provider: converter.CurrencyRates, base_currency: str, date_obj: date) -> []:
+        """
+        Fetch all rates for availbale currencies for this service
+        :param provider: Currency rates
+        :param base_currency: base currency
+        :param date_obj: date of value
+        :return: List of conversion rates
+        """
         try:
             _rates = provider.get_rates(base_cur=base_currency, date_obj=date_obj)
             return [self.serializer(
@@ -32,6 +45,14 @@ class ForexService(RateService):
                            provider: converter.CurrencyRates,
                            base_currency: str,
                            currency: str, date_obj: date) -> []:
+        """
+        Fetch one conversion rate between a currency and a base currency at a given date
+        :param provider: provider of the values
+        :param base_currency: base currency
+        :param currency: currency to convert from
+        :param date_obj: date of value
+        :return: List of rates
+        """
         try:
             value = provider.get_rate(dest_cur=currency, base_cur=base_currency, date_obj=date_obj)
             return [self.serializer(
@@ -49,6 +70,14 @@ class ForexService(RateService):
                     base_currency: str = settings.BASE_CURRENCY,
                     currency: str = None,
                     date_obj: date = date.today(), to_obj: date = None) -> []:
+        """
+        Get conversion rates between currency and base currency for a range of dates
+        :param base_currency: currency to convert to
+        :param currency: currency to convert from
+        :param date_obj: beginning of range
+        :param to_obj: end of range
+        :return: List of conversion rates
+        """
         c = converter.CurrencyRates()
         rates = []
         _rates = []
---
+++ src/geocurrency/units/apps.py
@@ -0,0 +1,8 @@
+# Geocurrency Units app config
+
+from django.apps import AppConfig
+
+
+class UnitsConfig(AppConfig):
+    name = "units"
+    verbose_name = "Units"
