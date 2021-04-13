25f42205ce9999b9b396c843d949e1cf7251ece7
fmeurou <fmeurou@wapp6.com>
2021-04-13 08:57:48 +0200

    Handle range of dates in management command

--- src/geocurrency/rates/management/commands/fetch_rates.py
+++ src/geocurrency/rates/management/commands/fetch_rates.py
@@ -1,7 +1,7 @@
 """
 Command to fetch rates for all currencies
 """
-
+from datetime import datetime, date
 from django.core.management.base import BaseCommand
 
 
@@ -15,7 +15,10 @@ class Command(BaseCommand):
         """
         Add date argument to the command
         """
-        parser.add_argument('date', nargs='*', type=str)
+        parser.add_argument("-f", '--from_date', type=str, help="Date in YYYY-MM-DD format."
+                                                                   "Start fetching from this date, defaults to today")
+        parser.add_argument("-t", '--to_date', type=str, help="Date in YYYY-MM-DD format."
+                                                                 "End of fetch range. defaults to today")
 
     def handle(self, *args, **options):
         """
@@ -23,7 +26,18 @@ class Command(BaseCommand):
         """
         from geocurrency.currencies.models import Currency
         from geocurrency.rates.models import Rate
+        today = date.today().strftime('%Y-%m-%d')
+        try:
+            from_date = datetime.strptime(options.get('from_date', today), '%Y-%m-%d')
+            to_date = datetime.strptime(options.get('to_date', today), '%Y-%m-%d')
+        except ValueError:
+            print("invalid dates")
+            exit(-1)
 
         for currency in Currency.all_currencies():
             self.stdout.write('fetching rates for currency {}.'.format(currency.code))
-            Rate.objects.fetch_rates(base_currency=currency.code)
+            Rate.objects.fetch_rates(
+                base_currency=currency.code,
+                date_obj=from_date,
+                to_obj=to_date
+            )
